6e2d06d1457d894bb17d5a5ecf4c0e3bb5d6a0f4
Alessio Rocco <alessio.rocco.lt@gmail.com>
2019-07-05 10:55:44 +0200

    Add graphiql-rails to simplify the dev process

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 Spree::Core::Engine.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/'
+  end
+
   post '/', to: 'graphql#execute'
 end
