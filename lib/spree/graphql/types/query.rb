0dd6ce95e7fa6a3cd5413b0dc6d2261bce769396
Christian Rimondi <christian.rimondi@gmail.com>
2019-07-12 15:17:45 +0200

    Add Relay global object identification
    This commit implements the required methods to support Relay features.
    More documentation about Relay and graphql-ruby can be found here:
    
    https://graphql-ruby.org/relay/object_identification.html

--- lib/spree/graphql/types/query.rb
+++ lib/spree/graphql/types/query.rb
@@ -10,6 +10,12 @@ module Spree
         def test_field
           "Hello World!"
         end
+
+        # Used by Relay to lookup objects by UUID:
+        field :node, field: GraphQL::Relay::Node.field
+
+        # Fetches a list of objects given a list of UUIDs
+        field :nodes, field: GraphQL::Relay::Node.plural_field
       end
     end
   end
---
+++ spec/lib/spree/graphql/schema_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Spree::Graphql::Schema do
+  describe '.id_from_object' do
+    subject { described_class.id_from_object(object, nil, nil) }
+
+    context 'when object responds to :id' do
+      let(:object) { build(:country, id: 1) }
+
+      after { subject }
+
+      it { expect(::GraphQL::Schema::UniqueWithinType).to receive(:encode).with('Spree::Country', 1) }
+    end
+
+    context 'when object does not respond to :id' do
+      let(:object) { nil }
+
+      it { expect { subject }.to raise_error(NoMethodError) }
+    end
+  end
+
+  describe '.object_from_id' do
+    subject { described_class.object_from_id('ID', nil) }
+
+    let!(:object) { create(:country) }
+    let(:klass) { 'Spree::Country' }
+    let(:id) { object.id }
+
+    before do
+      allow(::GraphQL::Schema::UniqueWithinType).to receive(:decode).and_return([klass, id])
+    end
+
+    context 'when the class and the id exist' do
+      it { is_expected.to eq object }
+    end
+
+    context 'when the class does not exist' do
+      let(:klass) { 'NotExistingClass' }
+
+      it { expect { subject }.to raise_error(NameError) }
+    end
+
+    context 'when the id does not exist' do
+      let(:id) { Spree::Country.maximum(:id).next }
+
+      it { expect { subject }.to raise_error(ActiveRecord::RecordNotFound) }
+    end
+  end
+
+  describe '.resolve_type' do
+    subject { described_class.resolve_type(nil, object, nil) }
+
+    context 'when graphql type exists' do
+      let(:object) { build(:country) }
+
+      # rubocop:disable Style/ClassAndModuleChildren
+      class Spree::Graphql::Types::Country < Spree::Graphql::Types::Base::RelayNode; end
+      # rubocop:enable Style/ClassAndModuleChildren
+
+      it { is_expected.to eq Spree::Graphql::Types::Country.graphql_definition }
+    end
+
+    context 'when graphql type does not exist' do
+      let(:object) { double(:not_existing) }
+
+      it { expect { subject }.to raise_error(NameError) }
+    end
+  end
+end
