280aa7a522e560a080553edf46750970389ee267
Christian Rimondi <christian.rimondi@gmail.com>
2019-07-12 15:17:45 +0200

    Add Relay global object identification
    This commit implements the required methods to support Relay features.
    More documentation about Relay and graphql-ruby can be found here:
    
    https://graphql-ruby.org/relay/object_identification.html

--- lib/spree/graphql/types/query.rb
+++ lib/spree/graphql/types/query.rb
@@ -4,6 +4,11 @@ module Spree
   module Graphql
     module Types
       class Query < Types::Base::Object
+        # Used by Relay to lookup objects by UUID:
+        field :node, field: GraphQL::Relay::Node.field
+
+        # Fetches a list of objects given a list of UUIDs
+        field :nodes, field: GraphQL::Relay::Node.plural_field
       end
     end
   end
