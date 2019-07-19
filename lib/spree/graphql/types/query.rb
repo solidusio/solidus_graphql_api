1db3fd6e5f4ef4a1dd9e59138fe235bca5354a01
Christian Rimondi <christian.rimondi@gmail.com>
2019-07-19 12:34:27 +0200

    Remove testField

--- lib/spree/graphql/types/query.rb
+++ lib/spree/graphql/types/query.rb
@@ -4,13 +4,6 @@ module Spree
   module Graphql
     module Types
       class Query < Types::Base::Object
-        # TODO: remove me
-        field :test_field, String, null: false,
-          description: "An example field added by the generator"
-        def test_field
-          "Hello World!"
-        end
-
         # Used by Relay to lookup objects by UUID:
         field :node, field: GraphQL::Relay::Node.field
 
