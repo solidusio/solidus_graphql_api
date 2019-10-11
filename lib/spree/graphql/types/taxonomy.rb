1165bb24aa005009c9b284f6f3e328d01a019c62
Christian Rimondi <christian.rimondi@gmail.com>
2019-10-11 15:38:37 +0200

    Add taxons connection to Taxonomy Type
    It adds the taxons connection field to the Taxonomy Type and uses the
    Spree::Queries::Taxonomy::TaxonsQuery query object with batch loader
    to retrieve the taxonomy taxons.

--- lib/spree/graphql/types/taxonomy.rb
+++ lib/spree/graphql/types/taxonomy.rb
@@ -7,8 +7,13 @@ module Spree
         description 'Taxonomy.'
 
         field :name, String, null: false
+        field :taxons, Types::Taxon.connection_type, null: false
         field :created_at, GraphQL::Types::ISO8601DateTime, null: true
         field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
+
+        def taxons
+          Spree::Queries::Taxonomy::TaxonsQuery.new(taxonomy: object).call
+        end
       end
     end
   end
--- spec/integration/taxonomies_spec.rb
+++ spec/integration/taxonomies_spec.rb
@@ -4,8 +4,21 @@ require 'spec_helper'
 
 RSpec.describe "Taxonomies" do
   include_examples 'query is successful', :taxonomies do
-    let!(:taxonomies) { create_list(:taxonomy, 2) }
+    let(:taxonomy_nodes) { subject.data.taxonomies.nodes }
+    let(:taxon_nodes) { taxonomy_nodes.map { |taxonomy_node| taxonomy_node.taxons.nodes }.flatten }
+    let(:child_nodes) { taxon_nodes.map { |taxon_node| taxon_node.children.nodes }.flatten }
 
-    it { expect(subject.data.taxonomies.nodes).to_not be_empty }
+    before do
+      taxonomies = create_list(:taxonomy, 2)
+      taxonomies.each do |taxonomy|
+        create(:taxon, taxonomy: taxonomy, parent: taxonomy.taxons.first)
+      end
+    end
+
+    it { expect(taxonomy_nodes).to be_present }
+
+    it { expect(taxon_nodes).to be_present }
+
+    it { expect(child_nodes).to be_present }
   end
 end
