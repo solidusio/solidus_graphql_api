d69bf45a5c4728eca6aba7fe731473e8403eafda
Alessio Rocco <alessio.rocco.lt@gmail.com>
2019-10-11 15:13:39 +0200

    Add Property Type

---
+++ lib/spree/graphql/types/property.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Spree
+  module Graphql
+    module Types
+      class Property < Base::RelayNode
+        description 'Property.'
+
+        field :created_at, GraphQL::Types::ISO8601DateTime, null: true
+        field :name, String, null: false
+        field :presentation, String, null: false
+        field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
+      end
+    end
+  end
+end
