21a69bcad0a3c0ae1136e34311188b3379c8df82
Alessio Rocco <alessio.rocco.lt@gmail.com>
2019-10-18 11:12:53 +0200

    Add product properties field to Product Type
    It adds the product properties connection field to the Product Type and
    uses the Spree::Queries::Product::ProductPropertiesQuery query object
    with batch loader to retrieve the product properties.

--- spec/integration/products_spec.rb
+++ spec/integration/products_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe "Products" do
     let(:master_variant_nodes) { product_nodes.map(&:masterVariant) }
     let(:option_type_nodes) { product_nodes.map { |product_node| product_node.optionTypes.nodes }.flatten }
     let(:option_value_nodes) { option_type_nodes.map { |option_type| option_type.optionValues.nodes }.flatten }
+    let(:product_properties_nodes) { product_nodes.map { |product_node| product_node.productProperties.nodes }.flatten }
+    let(:product_properties_property_nodes) { product_properties_nodes.map(&:property) }
     let(:variant_nodes) { product_nodes.map { |product_node| product_node.variants.nodes }.flatten }
     let(:variant_default_price_nodes) { variant_nodes.map(&:defaultPrice) }
     let(:variant_option_value_nodes) { variant_nodes.map { |variant_node| variant_node.optionValues.nodes }.flatten }
@@ -20,6 +22,7 @@ RSpec.describe "Products" do
         create(:option_value, option_type: option_type)
       end
       create_list(:variant, 2, product: product)
+      create(:product_property, product: product)
     end
 
     it { expect(product_nodes).to be_present }
@@ -29,6 +32,10 @@ RSpec.describe "Products" do
     it { expect(option_type_nodes).to be_present }
 
     it { expect(option_value_nodes).to be_present }
+
+    it { expect(product_properties_nodes).to be_present }
+
+    it { expect(product_properties_property_nodes).to be_present }
 
     it { expect(variant_nodes).to be_present }
 
