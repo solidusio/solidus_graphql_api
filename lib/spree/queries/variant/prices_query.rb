5e3e29a8f8282d4bc0a2fe970bf49c72022a85f7
Alessandro Desantis <desa.alessandro@gmail.com>
2019-10-18 17:59:32 +0200

    Implement the new batch-loading framework for Variant queries

--- lib/spree/queries/variant/prices_query.rb
+++ lib/spree/queries/variant/prices_query.rb
@@ -11,11 +11,7 @@ module Spree
         end
 
         def call
-          BatchLoader::GraphQL.for(variant.id).batch(default_value: []) do |variant_ids, loader|
-            Spree::Price.where(variant_id: variant_ids).each do |price|
-              loader.call(price.variant_id) { |memo| memo << price }
-            end
-          end
+          Spree::Graphql::BatchLoader.for(variant, :prices)
         end
       end
     end
