6c014a89ef0558fa6bcbde84d7f72a667f5a9736
Rainer Dema <rainer.dema@gmail.com>
2019-11-22 14:01:41 +0100

    Add all address fields to User Type
    - Adds:
       + A generic `addresses` node
       + `defaultAddress`
       + `billAddress `
     `Types::Address` fields to the User Type.
    - Update expected_schema with all address fields.

--- lib/solidus_graphql_api/types/user.rb
+++ lib/solidus_graphql_api/types/user.rb
@@ -5,11 +5,15 @@ module SolidusGraphqlApi
     class User < Base::RelayNode
       description 'User.'
 
+      field :addresses, Types::Address.connection_type, null: false
+      field :bill_address, Types::Address, null: true
       field :created_at, GraphQL::Types::ISO8601DateTime, null: true
       field :current_sign_in_at, GraphQL::Types::ISO8601DateTime, null: true
+      field :default_address, Types::Address, null: true
       field :email, String, null: false
       field :last_sign_in_at, GraphQL::Types::ISO8601DateTime, null: true
       field :login, String, null: true
+      field :ship_address, Types::Address, null: true
       field :sign_in_count, Integer, null: false
       field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
     end
--- spec/integration/current_user_spec.rb
+++ spec/integration/current_user_spec.rb
@@ -4,9 +4,18 @@ require 'spec_helper'
 
 RSpec.describe "Current User" do
   include_examples 'query is successful', :currentUser do
-    let(:user) { create(:user) }
+    let(:user) { create(:user_with_addresses) }
+    let(:address_nodes) { subject.data.currentUser.addresses.nodes }
+    let(:default_address) { subject.data.currentUser.defaultAddress }
+    let(:ship_address) { subject.data.currentUser.shipAddress }
+    let(:bill_address) { subject.data.currentUser.billAddress }
     let(:context) { Hash[current_user: user] }
 
     it { expect(subject.data.currentUser).to_not be_nil }
+
+    it { expect(address_nodes).to be_present }
+    it { expect(default_address).to be_present }
+    it { expect(ship_address).to be_present }
+    it { expect(bill_address).to be_present }
   end
 end
