6eb0074af54252363dbcea53c4fc924b94afe77f
Alessio Rocco <alessio.rocco.lt@gmail.com>
2019-11-23 11:54:18 +0100

    Use slug in productBySlug query specs

--- spec/integration/product_by_slug_spec.rb
+++ spec/integration/product_by_slug_spec.rb
@@ -4,6 +4,11 @@ require 'spec_helper'
 
 RSpec.describe "ProductBySlug" do
   include_examples 'query is successful', :product_by_slug do
-    let!(:product) { create(:product, slug: 'slug') }
+    let!(:product) { create(:product) }
+    let(:variables) { Hash[slug: product.slug] }
+
+    let(:product_id) { SolidusGraphqlApi::Schema.id_from_object(product, Spree::Product, {}) }
+
+    it { expect(subject.data.productBySlug.id).to eq product_id }
   end
 end
