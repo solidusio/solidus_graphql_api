8ba9849adf298eacc3ea837a8e8c283889ab280f
Alessio Rocco <alessio.rocco.lt@gmail.com>
2019-12-06 08:49:37 +0100

    Add states field specs on countries query

--- spec/integration/countries_spec.rb
+++ spec/integration/countries_spec.rb
@@ -14,6 +14,21 @@ RSpec.describe_query :countries do
       before { create(:country, id: 2, iso: 'IT') }
 
       it { is_expected.to match_response(:countries) }
+
+      connection_field :states, query: 'countries/states' do
+        context 'when states does not exists' do
+          it { is_expected.to match_response('countries/empty_states') }
+        end
+
+        context 'when states exists' do
+          before do
+            create(:state, id: 1, country: country)
+            create(:state, id: 2, country: country, state_code: 'CA')
+          end
+
+          it { is_expected.to match_response('countries/states') }
+        end
+      end
     end
   end
 end
