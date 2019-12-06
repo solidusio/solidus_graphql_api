621c98fe4366961d486090d62b00ac0a5e386f8f
Alessio Rocco <alessio.rocco.lt@gmail.com>
2019-12-06 08:46:37 +0100

    Add Timecop gem as a development dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ require 'pry'
 
 require "graphql/schema_comparator"
 require 'with_model'
+require 'timecop'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
