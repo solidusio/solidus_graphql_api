3904187dcb563b0fe607ca2811e0e613ca4b0074
Alessio Rocco <alessio.rocco.lt@gmail.com>
2020-01-17 22:20:00 +0100

    Use factories to create the current store in spec

--- spec/integration/current_store_spec.rb
+++ spec/integration/current_store_spec.rb
@@ -3,17 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe_query :currentStore, query: :current_store, freeze_date: true do
-  let!(:store) do
-    create(:store,
-           cart_tax_country_iso: 'US',
-           code: 'spree',
-           default_currency: 'USD',
-           meta_description: 'store description',
-           meta_keywords: 'store, metaKeywords',
-           name: 'Spree Test Store',
-           seo_title: 'Store Title',
-           url: 'www.example.com')
-  end
+  let!(:store) { create(:store, :with_defaults) }
 
   let(:query_context) { Hash[current_store: store] }
 
