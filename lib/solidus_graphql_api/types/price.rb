b53660aa2237c57ff27fab92bbc9647ed1d06226
Rainer Dema <rainer.dema@gmail.com>
2020-01-24 13:59:18 +0100

    Convert `currency` String field in Price Type to Currency Type

--- lib/solidus_graphql_api/types/price.rb
+++ lib/solidus_graphql_api/types/price.rb
@@ -8,14 +8,13 @@ module SolidusGraphqlApi
       field :amount, String, null: false
       field :country_iso, String, null: true
       field :created_at, GraphQL::Types::ISO8601DateTime, null: true
-      field :currency, String, null: false
-      field :currency_symbol, String, null: false
+      field :currency, Currency, null: false
       field :display_amount, String, null: false
       field :display_country, String, null: false
       field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
 
-      def currency_symbol
-        object.display_amount.currency.symbol
+      def currency
+        object.display_amount.currency
       end
     end
   end
