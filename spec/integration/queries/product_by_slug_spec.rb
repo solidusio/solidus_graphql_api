83d51aca58f36d86f2f4149d8303190aa3b59847
Christian Rimondi <christian.rimondi@gmail.com>
2020-02-07 16:21:25 +0100

    Improve product_by_slug integration spec
    Uses the 'describe_query' and 'connection_field' example groups to
    set the subject query and to freeze the time, and the 'match_response'
    custom matcher to compare the query with the result.

--- spec/integration/queries/product_by_slug_spec.rb
+++ spec/integration/queries/product_by_slug_spec.rb
@@ -2,8 +2,41 @@
 
 require 'spec_helper'
 
-RSpec.describe "ProductBySlug" do
-  include_examples 'query is successful', :product_by_slug do
-    let!(:product) { create(:product, slug: 'slug') }
+RSpec.describe_query :product_by_slug, query: :product_by_slug, freeze_date: true do
+  let!(:product) { create(:product, id: 1, name: 'Product', slug: 'slug', sku: 'SKU') }
+
+  let!(:option_type) { create(:option_type, id: 1, name: 'foo-size') }
+  let!(:option_value) { create(:option_value, id: 1, name: 'Size', option_type: option_type) }
+  let!(:product_option_type) { create(:product_option_type, id: 1, product: product, option_type: option_type) }
+
+  let!(:product_property) { create(:product_property, id: 1, product: product) }
+
+  let!(:first_variant) { create(:base_variant, product: product) }
+  let!(:second_variant) { create(:base_variant, product: product) }
+
+  before do
+    product.master.update!(option_values: [option_value])
+  end
+
+  field :productBySlug do
+    context "when the slug doesn't exist" do
+      let(:query_variables) { { slug: 'non-existent-slug' } }
+
+      it { expect(subject.dig(:data, :productBySlug)).to be_nil }
+    end
+
+    context 'when the slug exists' do
+      let(:query_variables) { { slug: 'slug' } }
+
+      let(:args) do
+        {
+          product: product,
+          first_variant: first_variant,
+          second_variant: second_variant
+        }
+      end
+
+      it { is_expected.to match_response(:product_by_slug).with_args(args) }
+    end
   end
 end
