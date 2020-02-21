1ecdd5c43d00cf6a5690b2cad4ff9acddddfff56
Christian Rimondi <christian.rimondi@gmail.com>
2020-02-21 11:08:06 +0100

    Add RansackJson argument type
    This custom JSON type must be used whenever we need an input argument to
    deal with ransack query.
    
    It receives a raw JSON input and converts all the relay style IDs
    (base64 encoded) to the corresponding IDs that can be used in SQL
    queries.

---
+++ lib/solidus_graphql_api/types/ransack_json.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module SolidusGraphqlApi
+  module Types
+    class RansackJson < GraphQL::Types::JSON
+      def self.coerce_input(value, _ctx)
+        value.each do |key, field|
+          case field
+          when String
+            value[key] = decode(field)
+          when Array
+            value[key] = decode_array(field)
+          end
+        end
+        value
+      end
+
+      def self.decode(value)
+        class_name, item_id = decode_if_relay_id(value)
+
+        return value unless class_exists?(class_name)
+
+        to_i_or_nil(item_id) || value
+      end
+
+      def self.decode_array(array)
+        array.map do |value|
+          value.is_a?(String) ? decode(value) : value
+        end
+      end
+
+      def self.decode_if_relay_id(value)
+        GraphQL::Schema::UniqueWithinType.decode(value)
+      rescue ArgumentError
+        [nil, nil]
+      end
+
+      def self.class_exists?(value)
+        return false if value.nil?
+
+        Object.const_defined?(value)
+      rescue NameError
+        false
+      end
+
+      def self.to_i_or_nil(value)
+        return value if value.nil?
+
+        Integer(value)
+      rescue ArgumentError
+        nil
+      end
+    end
+  end
+end
---
+++ spec/lib/solidus_graphql_api/types/ransack_json_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe SolidusGraphqlApi::Types::RansackJson do
+  describe '.coerce_input' do
+    subject { described_class.coerce_input(value, nil) }
+
+    let(:value) do
+      {
+        number: 1,
+        string: 'string',
+        id: 'U3ByZWU6OkNvdW50cnktMQ==', # Spree::Country-1
+        generic_base64: 'U3ByZWU6OkNvdW50cnktdGVzdA==', # Spree::Country-test
+        strings_array: [
+          'string1', 'string2'
+        ],
+        numbers_array: [
+          1, 2
+        ],
+        ids_array: [
+          'U3ByZWU6OkNvdW50cnktMg==', 'U3ByZWU6OkNvdW50cnktMw==' # Spree::Country-2, Spree::Country-3
+        ],
+        generic_base64_array: [
+          'VGVzdDE=', 'VGVzdDI=' # Test1, Test2
+        ]
+      }
+    end
+
+    let(:result) do
+      {
+        number: 1,
+        string: 'string',
+        id: 1,
+        generic_base64: 'U3ByZWU6OkNvdW50cnktdGVzdA==',
+        strings_array: [
+          'string1', 'string2'
+        ],
+        numbers_array: [
+          1, 2
+        ],
+        ids_array: [
+          2, 3
+        ],
+        generic_base64_array: [
+          'VGVzdDE=', 'VGVzdDI='
+        ]
+      }
+    end
+
+    it { is_expected.to eq(result) }
+  end
+end
