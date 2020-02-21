779a5dea63d106f53d24d932672b712a27907372
Alessio Rocco <alessio.rocco.lt@gmail.com>
2020-02-21 16:49:31 +0100

    Fix user sign in bug
    Before this commit, the service object "User::SignIn" returned the user
    even if the password was incorrect.

--- spec/integration/mutations/sign_in_spec.rb
+++ spec/integration/mutations/sign_in_spec.rb
@@ -6,26 +6,40 @@ RSpec.describe_mutation :sign_in, mutation: :sign_in do
   let(:email) { "john@example.com" }
   let(:password) { "secret" }
 
-  let(:mutation_variables) {
-    {
-      input: {
-        email: email,
-        password: password
-      }
-    }
-  }
+  let!(:user) { create(:user, email: 'john@example.com', password: 'secret') }
+
+  let(:mutation_variables) { Hash[input: { email: email, password: password }] }
 
   context "when given valid credentials" do
-    let!(:user) { create(:user, email: email, password: password) }
-
     it { expect(subject[:data][:signIn][:user][:email]).to eq(email) }
 
     it { expect(subject[:data][:signIn][:errors]).to be_empty }
   end
 
   context "when given invalid credentials" do
-    it { expect(subject[:data][:signIn][:user]).to be_nil }
+    context 'with wrong email' do
+      let(:email) { 'wrongemail@example.com' }
 
-    it { expect(subject[:data][:signIn][:errors]).to eq ["Invalid email or password."] }
+      it { expect(subject[:data][:signIn][:user]).to be_nil }
+
+      it { expect(subject[:data][:signIn][:errors]).to eq ["Invalid email or password."] }
+    end
+
+    context 'with wrong password' do
+      let(:password) { 'wrongpassword' }
+
+      it { expect(subject[:data][:signIn][:user]).to be_nil }
+
+      it { expect(subject[:data][:signIn][:errors]).to eq ["Invalid email or password."] }
+    end
+
+    context 'with wrong email and password' do
+      let(:email) { 'wrongemail@example.com' }
+      let(:password) { 'wrongpassword' }
+
+      it { expect(subject[:data][:signIn][:user]).to be_nil }
+
+      it { expect(subject[:data][:signIn][:errors]).to eq ["Invalid email or password."] }
+    end
   end
 end
