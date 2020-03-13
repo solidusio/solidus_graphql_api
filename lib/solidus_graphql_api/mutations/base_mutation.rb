f47390ac242df7eed432c7e5f3babeca798f341f
Alessio Rocco <alessio.rocco.lt@gmail.com>
2020-03-13 10:49:49 +0100

    Add BaseMutation helper methods
    The added helper method are:
    
      - current_user
      - current_ability
      - user_errors
    
    The 'current_user' and 'current_ability' delegates to the
    context, 'user_errors' is used to return 'errors as data' as
    described in the documentation:
    
    https://graphql-ruby.org/mutations/mutation_errors.html#errors-as-data

--- lib/solidus_graphql_api/mutations/base_mutation.rb
+++ lib/solidus_graphql_api/mutations/base_mutation.rb
@@ -7,6 +7,27 @@ module SolidusGraphqlApi
       field_class Types::Base::Field
       input_object_class Types::Base::InputObject
       object_class Types::Base::Object
+
+      private
+
+      def current_user
+        context[:current_user]
+      end
+
+      def current_ability
+        context[:current_ability]
+      end
+
+      def user_errors(*path, errors)
+        return [] if errors.empty?
+
+        errors.map do |attribute, message|
+          {
+            path: ["input", *path, attribute.to_s.camelize(:lower)],
+            message: message,
+          }
+        end
+      end
     end
   end
 end
