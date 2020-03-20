d6be8970417f3219b8c2c956d18dae7afca430b8
Rainer Dema <rainer.dema@gmail.com>
2020-03-20 11:50:41 +0100

    Fix nullable fields in `Store` type
    Based on presence validations in `Spree::Store`:
    https://github.com/solidusio/solidus/blob/d04ff9063e8048858acde5a8cac42933de3c28a3/core/app/models/spree/store.rb#L20-L23

--- lib/solidus_graphql_api/types/store.rb
+++ lib/solidus_graphql_api/types/store.rb
@@ -6,18 +6,18 @@ module SolidusGraphqlApi
       description 'Store.'
 
       field :cart_tax_country_iso, String, null: true
-      field :code, String, null: true
+      field :code, String, null: false
       field :created_at, GraphQL::Types::ISO8601DateTime, null: true
       field :currencies, Currency.connection_type, null: false
       field :default_currency, String, null: true
       field :default, Boolean, null: false
-      field :mail_from_address, String, null: true
+      field :mail_from_address, String, null: false
       field :meta_description, String, null: true
       field :meta_keywords, String, null: true
-      field :name, String, null: true
+      field :name, String, null: false
       field :seo_title, String, null: true
       field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
-      field :url, String, null: true
+      field :url, String, null: false
 
       def currencies
         Spree::Config.available_currencies
