25fcd1c4d8685dd999c43a8096a15041e608c1f1
Alessio Rocco <alessio.rocco.lt@gmail.com>
2020-04-10 13:22:03 +0200

    Add ShippingRates connection to Shipping type
    Creates the ShippingRates type and adds the connection to the
    Shipping type.

--- lib/solidus_graphql_api/types/shipment.rb
+++ lib/solidus_graphql_api/types/shipment.rb
@@ -8,10 +8,15 @@ module SolidusGraphqlApi
       field :created_at, GraphQL::Types::ISO8601DateTime, null: true
       field :number, String, null: false
       field :shipped_at, GraphQL::Types::ISO8601DateTime, null: true
+      field :shipping_rates, ShippingRate.connection_type, null: false
       field :state, String, null: false
       field :tracking, String, null: true
       field :tracking_url, String, null: true
       field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
+
+      def shipping_rates
+        Queries::Shipment::ShippingRatesQuery.new(shipment: object).call
+      end
     end
   end
 end
