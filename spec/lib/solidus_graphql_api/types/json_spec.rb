e7f6e9dd064d9ebfa80f93495c85e5516b2e7d32
Rainer Dema <rainer.dema@gmail.com>
2020-07-03 15:32:41 +0200

    Manage nested custom Json types
    Through the use of recursion, this allows us to decode and normalize
    all the IDs present in nested JSON objects.

--- spec/lib/solidus_graphql_api/types/json_spec.rb
+++ spec/lib/solidus_graphql_api/types/json_spec.rb
@@ -12,6 +12,16 @@ RSpec.describe SolidusGraphqlApi::Types::Json do
         string: 'string',
         id: 'U3ByZWU6OkNvdW50cnktMQ==', # Spree::Country-1
         generic_base64: 'U3ByZWU6OkNvdW50cnktdGVzdA==', # Spree::Country-test
+        hash: {
+          id: 'U3ByZWU6OlByb21vdGlvbkFjdGlvbi04', # Spree::PromotionAction-8
+          generic_base64: 'U3ByZWU6OkNvdW50cnktdGVzdA==', # Spree::Country-test
+          strings_array: [
+            'string1', 'string2'
+          ],
+          ids_array: [
+            'U3ByZWU6OkNvdW50cnktMg==', 'U3ByZWU6OkNvdW50cnktMw==' # Spree::Country-2, Spree::Country-3
+          ]
+        },
         strings_array: [
           'string1', 'string2'
         ],
@@ -23,6 +33,27 @@ RSpec.describe SolidusGraphqlApi::Types::Json do
         ],
         generic_base64_array: [
           'VGVzdDE=', 'VGVzdDI=' # Test1, Test2
+        ],
+        hash_array: [
+          {
+            ids_array: [
+              'U3ByZWU6OlByb21vdGlvbkFjdGlvbi02', 'U3ByZWU6OlByb21vdGlvbkFjdGlvbi04' # Spree::PromotionAction-6, Spree::PromotionAction-8
+            ],
+            ids_hash: {
+              id: 'U3ByZWU6OkFkanVzdG1lbnQtNQ==', # Spree::Adjustment-5
+              id_action_parameters: ActionController::Parameters.new(id: 'U3ByZWU6Ok9yZGVyLTI=') # Spree::Order-2
+            }
+          },
+          {
+            string: 'string',
+            strings_array: [
+              'string3', 'string4'
+            ],
+            numbers_array: [
+              3, 4
+            ],
+            id: 'U3ByZWU6Ok9yZGVyLTE=' # Spree::Order-1
+          }
         ]
       }
     end
@@ -33,6 +64,16 @@ RSpec.describe SolidusGraphqlApi::Types::Json do
         string: 'string',
         id: 1,
         generic_base64: 'U3ByZWU6OkNvdW50cnktdGVzdA==',
+        hash: {
+          id: 8,
+          generic_base64: 'U3ByZWU6OkNvdW50cnktdGVzdA==',
+          strings_array: [
+            'string1', 'string2'
+          ],
+          ids_array: [
+            2, 3
+          ]
+        },
         strings_array: [
           'string1', 'string2'
         ],
@@ -44,6 +85,27 @@ RSpec.describe SolidusGraphqlApi::Types::Json do
         ],
         generic_base64_array: [
           'VGVzdDE=', 'VGVzdDI='
+        ],
+        hash_array: [
+          {
+            ids_array: [
+              6, 8
+            ],
+            ids_hash: {
+              id: 5,
+              id_action_parameters: ActionController::Parameters.new(id: 2)
+            }
+          },
+          {
+            string: 'string',
+            strings_array: [
+              'string3', 'string4'
+            ],
+            numbers_array: [
+              3, 4
+            ],
+            id: 1
+          }
         ]
       }
     end
