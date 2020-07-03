fe3305dcdc63c0b47e6841b2b0812a74aba2ff7c
Rainer Dema <rainer.dema@gmail.com>
2020-07-03 09:20:09 +0200

    Add Adjustment interface
    It adds the `Adjustment` interface and implements it in the new
    `TaxAdjustment` and `PromotionAdjustment` types.
    These are declared as `orphan_types` because they are never used
    directly but only connected through the `Adjustment` interface.
    
    For more information about this:
    https://graphql-ruby.org/type_definitions/interfaces.html#orphan-types

--- lib/solidus_graphql_api/types/tax_adjustment.rb
+++ lib/solidus_graphql_api/types/tax_adjustment.rb
@@ -3,13 +3,9 @@
 module SolidusGraphqlApi
   module Types
     class TaxAdjustment < Base::RelayNode
-      description 'TaxAdjustment.'
+      implements Types::Interfaces::Adjustment
 
-      field :amount, String, null: false
-      field :created_at, GraphQL::Types::ISO8601DateTime, null: true
-      field :eligible, Boolean, null: false
-      field :label, String, null: false
-      field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
+      description 'TaxAdjustment.'
     end
   end
 end
