b5c82bfbb01f35bac8393b1c3c4d2f59766dbe3e
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-06-18 10:29:35 -0600

    Updated factories loading after recent changes in solidus
    This commit reflects changes made by Solidus [PR #169]
    (solidusio/solidus_dev_support#169 (comment)) When testing extensions
    using Solidus vesions that support the definition_file_paths,
    it will load the core factory first and then the ones defined in
    `lib/extension_name/testing_support/factories`.

--- lib/solidus_graphql_api/testing_support/factories.rb
+++ lib/solidus_graphql_api/testing_support/factories.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
+require 'solidus_graphql_api/testing_support/factories/address_factory'
+require 'solidus_graphql_api/testing_support/factories/country_factory'
+require 'solidus_graphql_api/testing_support/factories/store_factory'
+require 'solidus_graphql_api/testing_support/factories/taxonomy_factory'
+
 FactoryBot.define do
 end
