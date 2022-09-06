fdce3a1b7c48cec747b112806d2fea9a5cc3d017
Martin Samami <m@massd.org>
2022-09-06 11:20:03 -0400

    mount engine at install with routes

--- lib/generators/solidus_graphql_api/install/install_generator.rb
+++ lib/generators/solidus_graphql_api/install/install_generator.rb
@@ -14,6 +14,19 @@ module SolidusGraphqlApi
         run 'bin/rails railties:install:migrations FROM=solidus_graphql_api'
       end
 
+      def mount_engine
+        insert_into_file File.join('config', 'routes.rb'), after: "Rails.application.routes.draw do\n" do
+          <<~HEREDOC
+            mount SolidusGraphqlApi::Engine, at: '/graphql'
+
+            # if Rails.env.development?
+              # Add gem 'graphql_playground-rails' inside your Gemfile to enable playground
+              # mount GraphqlPlayground::Rails::Engine, at: '/playground', graphql_path: '/graphql'
+            # end
+          HEREDOC
+        end
+      end
+
       def run_migrations
         run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]')) # rubocop:disable Layout/LineLength
         if run_migrations
