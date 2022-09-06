5d76ced4ece02e2e622346de8fcc07756385c78d
Martin Samami <m@massd.org>
2022-09-06 11:25:11 -0400

    update HEREDOC with indent

--- lib/generators/solidus_graphql_api/install/install_generator.rb
+++ lib/generators/solidus_graphql_api/install/install_generator.rb
@@ -16,7 +16,7 @@ module SolidusGraphqlApi
 
       def mount_engine
         insert_into_file File.join('config', 'routes.rb'), after: "Rails.application.routes.draw do\n" do
-          <<~HEREDOC
+          <<-HEREDOC.strip_heredoc.indent(2)
             mount SolidusGraphqlApi::Engine, at: '/graphql'
 
             # if Rails.env.development?
